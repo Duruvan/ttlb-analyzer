0271748a33058fd8a8b5c02af18a891f16023f08
Duruvan <37856017+Duruvan@users.noreply.github.com>
2018-03-28 19:50:16 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import csv
 from ttlb_analyzer import ttlb_analyzer
 
 def main():
+    #reading the csv file
     with open('exercise.log.csv') as file:
         readFile = csv.reader(file, delimiter=',')
         obj_sizes = []
@@ -17,18 +18,22 @@ def main():
         
         next(readFile) #skip the header row
         for row in readFile:
-            obj_size = int(row[1])
+            #typecasting each value to an integer type
+            obj_size = int(row[1]) 
             req_time = int(row[2])
             transfer_time = int(row[3])
             turn_time = int(row[5])
             
+            #building the input lists
             obj_sizes.append(obj_size)
             req_times.append(req_time)
             transfer_times.append(transfer_time)
             turn_times.append(turn_time)           
-        
+      
+    #initializing the analyzer class
     analyzer = ttlb_analyzer(obj_sizes, req_times, transfer_times, turn_times)
     
+    #visualizing the ttlb distribution
     analyzer.plot_distribution()
     
 if __name__ == "__main__":
--- ttlb_analyzer.py
+++ ttlb_analyzer.py
@@ -7,7 +7,6 @@ Created on Tue Mar 27 22:47:12 2018
 from collections import Counter
 import matplotlib.pyplot as plt
 import math
-import matplotlib
 import matplotlib.patches as mpatches
 
 class ttlb_analyzer:
@@ -29,56 +28,70 @@ class ttlb_analyzer:
 
     def plot_distribution(self):
         """
+        Visualization of the time to last byte per object size class
         """
         num_objects = len(self.ttlb)
-        ttlb_percentages = []
-        ttlb_values = []
-        size_classes = self.create_size_classes()
-        ttlb_classes = list(zip(size_classes, self.ttlb))
-        ttlb_classes_count = Counter(tuple(item) for item in ttlb_classes)
+        ttlb_percentages = [] #holds the % of ttlb time values 
+        ttlb_values = [] #holds the ttlb values
+        color_codes = self.create_color_codes() 
+        ttlb_classes = list(zip(color_codes, self.ttlb))
+        ttlb_classes_count = Counter(tuple(item) for item in ttlb_classes) #number of time each ttlb occurs in the distribution
         
+        #retrieving the ttlb values and color codes as a 2d list and ttlb percentages in another list
         for i,j in ttlb_classes_count.items():
             ttlb_values.append(i)
             ttlb_percentages.append((j/num_objects) * 100)
-                
-        size_classes, ttlb_values = zip(*ttlb_values)
-        
+                  
+        #retrieving the color codes and the corresponding ttlb values as two separate lists
+        color_codes, ttlb_values = zip(*ttlb_values)
         ttlb_values = list(ttlb_values)
         
-        for i in range(0, len(ttlb_values)):
-            if (ttlb_values[i] != 0):
-                ttlb_values[i] = math.log2(ttlb_values[i])
-    
-        lists = sorted(zip(*[ttlb_values, ttlb_percentages]))
-        ttlb_values, ttlb_percentages = list(zip(*lists))
+        #changing the ttlb values to a logarithmic scale base 2        
+        ttlb_values = [math.log2(i) if i !=0 else i for i in ttlb_values]
         
-        classes = ['<100K', '>100K<1M', '>1M<10M', '>10M<100M', '>100M<1G']
-        colors = ['red', 'green', 'blue', 'orange', 'purple']
+        #sorting the lists
+        lists = sorted(zip(*[ttlb_values, ttlb_percentages, color_codes]))
+        ttlb_values, ttlb_percentages, color_codes = list(zip(*lists))
         
-        plt.figure(figsize=(10,10))
-        barlist = plt.bar(ttlb_values, ttlb_percentages, width = 0.25)
-        for i in range (0, len(barlist)):
-            barlist[i].set_color(size_classes[i])
-        #plt.xlim(0, max(ttlb_values))
+        #visualizing the ttlb distribution
+        plt.figure(figsize=(12,12))
+        plt.scatter(ttlb_values, ttlb_percentages, c=color_codes)
+        
+        #barlist = plt.bar(ttlb_values, ttlb_percentages, width = 0.25)
+        #setting a limit on the x-axis
+        plt.xlim(0,max(ttlb_values))
+        #for i in range (0, len(barlist)):
+            #barlist[i].set_color(color_codes[i])
+            
+        #adding labels based on color
+        red_patch = mpatches.Patch(color='red', label='>100K')
+        green_patch = mpatches.Patch(color='green', label='>100K<1M')
+        blue_patch = mpatches.Patch(color='blue', label='>1M<10M')
+        yellow_patch = mpatches.Patch(color='yellow', label='>10M<100M')
+        cyan_patch = mpatches.Patch(color='cyan', label='>100M<1G')
+        plt.legend(handles=[red_patch, green_patch, blue_patch, yellow_patch, cyan_patch])
+        
+        #adding labels for both axes
         plt.xlabel('TTLB in ms (logarithmic scale base 2)')
         plt.ylabel('% of TTLB times in sample')
         plt.show()
         
-    def create_size_classes(self):
+    def create_color_codes(self):
         """
+        Returns a list that contains an RGBA color code based on the object size
         """
-        size_classes = []
+        color_codes = []
         
         for i in range(0,len(self.obj_sizes)):
             if self.obj_sizes[i] in range(0,100000):
-                size_classes.append('r')
+                color_codes.append('r')
             elif self.obj_sizes[i] in range(100000, 1000000):
-                size_classes.append('g')
+                color_codes.append('g')
             elif self.obj_sizes[i] in range(1000000, 10000000):
-                size_classes.append('b')
+                color_codes.append('b')
             elif self.obj_sizes[i] in range(10000000, 100000000):
-                size_classes.append('k')
+                color_codes.append('y')
             else:
-                size_classes.append('c')
+                color_codes.append('c')
                 
-        return size_classes
+        return color_codes
